d75cd91a1088d42a148925efed5373ac91d5d414
Dipankar Mitra <78472306+IamDipankar@users.noreply.github.com>
2025-01-15 16:30:22 +0600

    Create resetLogic.js

---
+++ resetLogic.js
@@ -0,0 +1,18 @@
+// Functionality to reset
+
+function reset(){
+    score1 = 0;
+    score2 = 0;
+    document.querySelector("#sc1").innerHTML = score1;
+    document.querySelector("#sc2").innerHTML = score2;
+    document.querySelector("h1").innerHTML = "Press Roll or Spacebar";
+}
+
+
+document.addEventListener("keydown", (event) => {
+    if (event.altKey){
+        if (event.key === "r" || event.key === "R") {
+            reset();
+        }
+    }
+});
